4c2ea9c56e2bf8062a859b00f7b76c4e1472da99
avanish-1312 <valavanavanish@gmail.com>
2026-07-12 16:55:53 +0530

    Connect backend to MongoDB Atlas

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,17 @@
+import dns from "dns";
+
+dns.setDefaultResultOrder("ipv4first");
+
 import dotenv from "dotenv";
 import app from "./app.js";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
+
+// Connect to MongoDB
+await connectDB();
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
